b86ff8d5d208b30f7fd943a666bf98aad0e0a48d
MK <49213568+Michaelxk-CodingSamurai@users.noreply.github.com>
2023-08-16 11:55:37 +0700

    Second change

--- src/Main.js
+++ src/Main.js
@@ -1,21 +1,36 @@
 import React, { Component } from "react";
- 
+import {
+    Route,
+    Routes,
+    NavLink,
+    HashRouter
+} from "react-router-dom";
+import Home from "./Home";
+import Reader from "./Reader";
+import Contact from "./Contact";
+
 class Main extends Component {
-  render() {
-    return (
-        <div>
-          <h1>Simple SPA</h1>
-          <ul className="header">
-            <li><a href="/">Home</a></li>
-            <li><a href="/stuff">Stuff</a></li>
-            <li><a href="/contact">Contact</a></li>
-          </ul>
-          <div className="content">
-             
-          </div>
-        </div>
-    );
-  }
+    render() {
+        return (
+            <HashRouter>
+                <div>
+                    <h1>Simple SPA</h1>
+                    <ul className="header">
+                        <li><NavLink to="/">Home</NavLink></li>
+                        <li><NavLink to="/reader">Reader</NavLink></li>
+                        <li><NavLink to="/contact">Contact</NavLink></li>
+                    </ul>
+                    <div className="content">
+                        <Routes>
+                            <Route exact path="/" element={ <Home /> }/>
+                            <Route path="/reader" element={ <Reader /> }/>
+                            <Route path="/contact" element= {<Contact /> }/>
+                        </Routes>
+                    </div>
+                </div>
+            </HashRouter>
+        );
+    }
 }
- 
+
 export default Main;
